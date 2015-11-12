d999463f6e8b9be6d12a19039d9337b54b15f5cf
Jason L. Walker <jasonlanewalker@gmail.com>
2015-11-11 20:27:49 -0600

    tweaked RouteFactoryInterface docblock

--- src/Interfaces/RouteFactoryInterface.php
+++ src/Interfaces/RouteFactoryInterface.php
@@ -12,13 +12,16 @@ namespace Fusion\Router\Interfaces;
 interface RouteFactoryInterface
 {
     /**
-     * Factory method to make a RouteInterface instance.
+     * Factory method to make a `RouteInterface` instance.
      *
+     * @see \Fusion\Router\Interfaces\RouteInterface
      * @param string $pattern The pattern used for matching a target.
-     * @param mixed $action Action that will be taken when this Route is selected
-     *     as a match.
-     * @param array $methods Array list of HTTP methods the Route can respond to.
-     * @param array $parameters Array list of parameters for the Route.
+     * @param mixed $action Action that will be taken when a `RouteInterface`
+     *     instance is selected as a match.
+     * @param array $methods Array list of HTTP methods the `RouteInterface`
+     *     instance can respond to.
+     * @param array $parameters Array list of parameters for the `RouteInterface`
+     *     instance.
      * @return \Fusion\Router\Interfaces\RouteInterface
      */
     public function make($pattern, $action = null, array $methods = [], array $parameters = []);
